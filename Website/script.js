601a3e0aa5444c19e961aeb1c708871c3bd877dc
JulienLoon <96742754+JulienLoon@users.noreply.github.com>
2024-12-17 20:53:08 +0100

    Green

--- Website/script.js
+++
@@ -1,5 +0,0 @@
-// Add event listener for button click
-document.getElementById('actionButton').addEventListener('click', function() {
-    alert('You clicked the button in the Blue Environment!');
-  });
-  
