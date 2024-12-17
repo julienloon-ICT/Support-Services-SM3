c4334d61e9bae5226b0466e08cffe78ee49c7a60
JulienLoon <96742754+JulienLoon@users.noreply.github.com>
2024-12-17 16:26:16 +0100

    Add Dockerfile for Blue-Green Deployment

---
+++ Website/script.js
@@ -0,0 +1,5 @@
+// Add event listener for button click
+document.getElementById('actionButton').addEventListener('click', function() {
+    alert('You clicked the button in the Blue Environment!');
+  });
+  
